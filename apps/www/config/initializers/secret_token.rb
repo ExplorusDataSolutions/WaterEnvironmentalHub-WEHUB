9fac81bb499f87b32a4971846493945459099d47
AdamDotCom <adam@kahtava.com>
2011-01-17 15:09:43 -0700

    preliminary directory structure naming (services, apps)

---
+++ apps/www/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Search::Application.config.secret_token = 'de1a86ae5fc664ba10f28360d68df548e6a3b8b2fee25de3d27a4c79c84e8bc0f4e846b1f2af8ffa3fe110d03fbc1e947433fd83f71f39b68118ac7a6ed36235'
