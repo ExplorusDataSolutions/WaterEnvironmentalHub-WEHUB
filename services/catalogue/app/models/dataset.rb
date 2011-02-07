5fde77413c9ea807fbea7b056551f017e9fc09a5
AdamDotCom <adam@kahtava.com>
2011-02-07 10:58:00 -0700

    Added custom finder for uuid lookups and test

--- services/catalogue/app/models/dataset.rb
+++ services/catalogue/app/models/dataset.rb
@@ -3,4 +3,6 @@ class Dataset < ActiveRecord::Base
   has_one :dataset_group
   has_many :features
   
+  scope :find_by_uuid, lambda {|uuid| { :conditions => { :uuid => uuid }}}
+  
 end
--- services/catalogue/spec/models/dataset_spec.rb
+++ services/catalogue/spec/models/dataset_spec.rb
@@ -16,5 +16,15 @@ describe Dataset do
     dataset.features.count.should > 1
     dataset.features[0].should be_kind_of(Feature)
   end  
+
+  it "can find by uuid"  do
+    set1 = Factory(:dataset, :name => "set1", :uuid => "01572180-32e2-11e0-bc8e-0800200c9a66")
+    set2 = Factory(:dataset, :name => "set2", :uuid => "0de453a0-32e2-11e0-bc8e-0800200c9a66")
+    set3 = Factory(:dataset, :name => "set3", :uuid => "2245a730-32e3-11e0-bc8e-0800200c9a66")
+    
+    Dataset.all.should == [set1, set2, set3]
+    
+    result = Dataset.find_by_uuid("0de453a0-32e2-11e0-bc8e-0800200c9a66").should == [set2]
+  end
   
 end
