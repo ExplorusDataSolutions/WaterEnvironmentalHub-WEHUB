8b70dffe84b778f54b2300fa1dc9581d165196d3
AdamDotCom <adam@kahtava.com>
2011-03-03 15:51:48 -0700

    DRY'd up the importer

--- console/geonetwork/importer.rb
+++ console/geonetwork/importer.rb
@@ -1,13 +1,14 @@
 require 'net/http'
 
 class Importer
-  attr_accessor :cookies, :server_address, :temp_directory, :username, :password
+  attr_accessor :cookies, :server_address, :temp_directory, :username, :password, :boundary
   
   def initialize(temp_directory='tmp/mefs', server_address='174.129.10.37:8080', username='development', password='development')
     @server_address = server_address
     @temp_directory = temp_directory
     @username = username
     @password = password
+    @boundary = '----RubyMultipartClient' + rand(1000000).to_s + 'ZZZZZ'
   end  
 
   def authenticate()
@@ -25,76 +26,48 @@ class Importer
     puts response.body
   end
   
-  def upload_mef(file)
-=begin
-    ToDo: DRY this method up
-=end    
-    puts "Uploading MEF to GeoNetwork"    
-    
-    boundary = '----RubyMultipartClient' + rand(1000000).to_s + 'ZZZZZ'
-    
+  def build_post(file)  
     post_body = []        
-    post_body << "--#{boundary}\r\n"
+    post_body << "--#{@boundary}\r\n"
 
-    post_body << "Content-Disposition: form-data; name=\"insert_mode\"\r\n"
-    post_body << "\r\n"    
-    post_body << "1\r\n"
-    post_body << "--#{boundary}\r\n"
-
-    post_body << "Content-Disposition: form-data; name=\"file_type\"\r\n"
-    post_body << "\r\n"    
-    post_body << "mef\r\n"
-    post_body << "--#{boundary}\r\n"
-
-    post_body << "Content-Disposition: form-data; name=\"data\"\r\n"
-    post_body << "\r\n"    
-    post_body << "\r\n"
-    post_body << "--#{boundary}\r\n"
-
+    form_values = { :insert_mode => '1', 
+                    :file_type => 'mef', 
+                    :data => '', 
+                    :template => 'n', 
+                    :title => '', 
+                    :uuidAction => 'overwrite', 
+                    :styleSheet => '_none_', 
+                    :group => '2', 
+                    :category => '_none'}
+                    
+    form_values.each do |key, value|
+      post_body << "Content-Disposition: form-data; name=\"#{key}\"\r\n"
+      post_body << "\r\n"    
+      post_body << "#{value}\r\n"
+      post_body << "--#{@boundary}\r\n"      
+    end
+      
     post_body << "Content-Disposition: form-data; name=\"mefFile\"; filename=\"#{File.basename(file)}\"\r\n"
     post_body << "Content-Type: application/octet-stream\r\n"
     post_body << "\r\n" 
   	content = open(file,"rb") do |f|
   		f.read()
   	end
-    post_body << content
-    post_body << "\r\n"
-    post_body << "--#{boundary}\r\n"  
-
-    post_body << "Content-Disposition: form-data; name=\"template\"\r\n"
-    post_body << "\r\n"    
-    post_body << "n\r\n"
-    post_body << "--#{boundary}\r\n"
-
-    post_body << "Content-Disposition: form-data; name=\"title\"\r\n"
-    post_body << "\r\n"    
-    post_body << "\r\n"
-    post_body << "--#{boundary}\r\n"
-
-    post_body << "Content-Disposition: form-data; name=\"uuidAction\"\r\n"
-    post_body << "\r\n"    
-    post_body << "overwrite\r\n"
-    post_body << "--#{boundary}\r\n"
-
-    post_body << "Content-Disposition: form-data; name=\"styleSheet\"\r\n"
-    post_body << "\r\n"    
-    post_body << "_none_\r\n"
-    post_body << "--#{boundary}\r\n"
-
-    post_body << "Content-Disposition: form-data; name=\"group\"\r\n"
-    post_body << "\r\n"    
-    post_body << "2\r\n"
-    post_body << "--#{boundary}\r\n"
-
-    post_body << "Content-Disposition: form-data; name=\"category\"\r\n"
-    post_body << "\r\n"    
-    post_body << "_none_\r\n"
-    post_body << "--#{boundary}--\r\n"
+  	post_body << content
+  	post_body << "\r\n"
+    post_body << "--#{@boundary}--\r\n"    
+    
+    post_body
+  end
+  
+  def upload_mef(file)
+    puts "Uploading MEF to GeoNetwork"    
 
     url = URI.parse("http://#{server_address}/geonetwork/srv/en/mef.import")
-    request = Net::HTTP::Post.new(url.path)      
-    request.body = post_body.join  
-    request.content_type = "multipart/form-data, boundary=#{boundary}"    
+    request = Net::HTTP::Post.new(url.path)
+    puts build_post(file).join
+    request.body = build_post(file).join
+    request.content_type = "multipart/form-data, boundary=#{@boundary}"    
     request['cookie'] = @cookies    
     response = Net::HTTP.start(url.host, url.port) {|http| http.request(request)}
 
@@ -105,6 +78,7 @@ class Importer
     authenticate
     
     mef_files = Dir.glob("#{Dir.getwd}/#{temp_directory}/*.mef")
+    puts "#{mef_files} to be uploaded"
     mef_files.each do |file|
       upload_mef(file)
     end
