8d5db99c05b07da77097a47eacedf6e8835ad99f
AdamDotCom <adam@kahtava.com>
2011-03-28 09:07:34 -0600

    Added logic to refresh search cache when GeoNetwork's MEF import is complete

--- consoles/geonetwork/do_import.rb
+++ consoles/geonetwork/do_import.rb
@@ -5,3 +5,9 @@ temp_directory = 'tmp/mefs'
 
 MefFactory.new(temp_directory).build_all
 Importer.new(temp_directory).upload_all
+
+puts "Refreshing cache"
+url = URI.parse('http://localhost:3001/search/refresh')
+http = Net::HTTP.new(url.host, url.port)
+response = http.start {|http| http.get(url.to_s) }
+puts response.body
