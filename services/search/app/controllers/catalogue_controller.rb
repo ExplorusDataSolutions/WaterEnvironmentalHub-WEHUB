6b89685233f29ff42c02616c32dd54b503ac7a84
bijeesh <bijeesh@rainconcert.in>
2011-05-17 15:56:54 +0530

    WE Catalogue-Detail View- comments page integrate

--- services/search/app/controllers/catalogue_controller.rb
+++ services/search/app/controllers/catalogue_controller.rb
@@ -20,4 +20,19 @@ class CatalogueController < ApplicationController
     
     @rating         = (4*138)/5; 
   end
+  
+  def comment
+    col             = File.read('public/my_collection.json')
+    @my_collection  = JSON.parse(col)
+    
+    recently        = File.read('public/recently_view.json')
+    @recently_view  = JSON.parse(recently)
+    
+    @breadcrumb     = Array.new
+    @breadcrumb[0]  = 'WE Catalogue'
+    @breadcrumb[1]  = 'Detail View'
+    @main_menu      = 'we_catalogue'
+    
+    @rating         = (4*138)/5; 
+  end
 end
