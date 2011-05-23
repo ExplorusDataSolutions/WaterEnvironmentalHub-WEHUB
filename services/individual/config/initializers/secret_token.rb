daf9e5566dd7104c6cba8f347e38292a9d0de662
AdamDotCom <adam@kahtava.com>
2011-05-23 14:21:07 -0600

    Cleanup, removed community and individual, EngineY takes care of most of this

--- services/individual/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Individual::Application.config.secret_token = '0406d0423d67ad6ecad03fa9cc56877a2649048dbbe981970854bfd3d69781853639b7ecda1f730e644f7b0c697fc8df9b50d75b2294ffbb332eb1b2e932e4c2'
