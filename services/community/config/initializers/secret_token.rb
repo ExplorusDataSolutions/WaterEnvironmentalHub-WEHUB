8e56b8967d205830aee1b85606a54ef810ec78fa
AdamDotCom <adam@kahtava.com>
2011-05-23 13:39:37 -0400

    Cleanu: removed the community service, EngineY replaced this project

--- services/community/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Community::Application.config.secret_token = 'cda2ad661962824c883ee04738d252c5122b54c4a146dcc08f4ab87894ba2de1fa882b2fcf0266010dbc56cbe26a7032d07c0a3ed944ba64c2dd131b2e5e629b'
