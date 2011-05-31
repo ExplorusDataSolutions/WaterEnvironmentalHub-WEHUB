f07fecdb75e75bdeb5d68ed8499d9949358f1563
bijeesh <bijeesh@rainconcert.in>
2011-05-31 17:58:54 +0530

    We data map page design

--- services/search/app/controllers/tools_controller.rb
+++ services/search/app/controllers/tools_controller.rb
@@ -6,9 +6,12 @@ class ToolsController < ApplicationController
   end
   
   def chart
-    @breadcrumb     = Array.new
-    @breadcrumb[0]  = 'WE Tools'
-    @breadcrumb[1]  = 'WE Data Graph'
+    @breadcrumb     = ['WE Tools','We Data Graph']
+    @main_menu      = 'we_tools'
+  end
+  
+  def map
+    @breadcrumb     = ['WE Tools','We Data Map']
     @main_menu      = 'we_tools'
   end
 
