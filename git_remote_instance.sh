abad3fae29a2ef38d278cd35bf8f6c178024d9eb
AdamDotCom <adam@kahtava.com>
2011-06-15 18:08:47 -0600

    Updated shell script for git pull

--- git_remote_instance.sh
+++ git_remote_instance.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ssh -i WE-Rails1.openssh 50.19.106.48 "cd /projects/WEHub ; git pull"
+ssh -i WE-Rails1.openssh root@ec2-50-19-106-48.compute-1.amazonaws.com "cd /projects/WEHub ; git pull"
