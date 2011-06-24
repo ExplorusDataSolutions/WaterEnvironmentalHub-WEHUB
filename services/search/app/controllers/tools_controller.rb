8b088bff02c0bbb316e3d35ccbd178dd19762f2d
bijeesh <bijeesh@rainconcert.in>
2011-06-24 17:22:05 +0530

    Updated the /tools/chart,/tools/map and /user/profile pages

--- services/search/app/controllers/tools_controller.rb
+++ services/search/app/controllers/tools_controller.rb
@@ -6,6 +6,7 @@ class ToolsController < ApplicationController
   end
   
   def chart
+    @id = params[:id]
     f = File.read('public/chart.json')
     @json = f.strip
     @breadcrumb     = ['WE Tools','We Data Graph']
@@ -18,6 +19,7 @@ class ToolsController < ApplicationController
   end
   
   def map
+    @id = params[:id]
     f = File.read('public/6.json')
     @json = f.strip
     @breadcrumb     = ['WE Tools','We Data Map']
@@ -25,8 +27,19 @@ class ToolsController < ApplicationController
   end
   
   def developers
+    @id = params[:id]
     @breadcrumb     = ['WE Tools','Developers']
     @main_menu      = 'we_tools'
+  end
+  
+  def add_new_dataset
+    @id     = params[:hid_last_cnt]
+    render :layout => false
+  end
+  
+  def add_new_dataset_map
+    @id     = params[:hid_last_cnt]
+    render :layout => false
   end
 
 end
--- services/search/public/javascripts/application.js
+++ services/search/public/javascripts/application.js
@@ -10,3 +10,78 @@ WEHub.dialog = function(title, message) {
     title: title
   });
 }
+
+function add_new_dataset(){
+	var hid_last_cnt	=	$('#hid_last_cnt').val()*1 + 1;
+	$('#hid_last_cnt').val(hid_last_cnt);
+	$.ajax({
+      url: "/tools/add_new_dataset/",
+      type:'POST',
+      dataType:'html',
+      data:{"hid_last_cnt": hid_last_cnt},
+      success: function(data) {
+                          $('#filter-content').append(data);
+                      },
+      error: function (data){alert("An error occurred:");}
+    });
+}
+
+function remove_dataset(){
+	jQuery("input[name=filter-check]:checked").each
+    (
+      function()
+      {
+	  	var id 	=	jQuery(this).val();
+		$('#filter-' + id).remove();
+      }
+    );
+}
+
+function add_new_dataset_map(){
+	var hid_last_cnt	=	$('#hid_last_cnt').val()*1 + 1;
+	$('#hid_last_cnt').val(hid_last_cnt);
+	$.ajax({
+      url: "/tools/add_new_dataset_map/",
+      type:'POST',
+      dataType:'html',
+      data:{"hid_last_cnt": hid_last_cnt},
+      success: function(data) {
+                          $('#filter-content').append(data);
+                      },
+      error: function (data){alert("An error occurred:");}
+    });
+}
+
+function remove_dataset_map(){
+	jQuery("input[name=filter-check]:checked").each
+    (
+      function()
+      {
+	  	var id 	=	jQuery(this).val();
+		$('#filter-' + id).remove();
+      }
+    );
+}
+
+
+function profile_about_me_edit(){
+	if ($('#about-me-edit').css('display') == 'none'){
+		$('#about-me-content').hide(1000);
+		$('#about-me-edit').show(1000)
+	}else{
+		$('#about-me-edit').hide(1000);
+		$('#about-me-content').show(1000);
+	}
+}
+function profile_skills_edit(){
+	if ($('#skills-edit').css('display') == 'none'){
+		$('#skills-content').hide(1000);
+		$('#skills-edit').show(1000)
+	}else{
+		$('#skills-edit').hide(1000);
+		$('#skills-content').show(1000);
+	}
+}
+function close_friends(friends_cnt){
+	$('#profile-friend-'+friends_cnt).hide(1000);
+}
