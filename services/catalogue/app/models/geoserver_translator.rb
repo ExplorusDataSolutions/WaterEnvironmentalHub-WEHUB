42a167acadf0346c66cf196e5dee898c8e72491e
AdamDotCom <adam@kahtava.com>
2011-07-28 09:49:22 -0600

    Caching for geoserver data

--- services/catalogue/app/models/geoserver_translator.rb
+++ services/catalogue/app/models/geoserver_translator.rb
@@ -2,35 +2,54 @@ require 'net/http'
 require 'active_support/core_ext'
 
 class GeoServerTranslator
-  attr_accessor :server_address, :timeout
+  attr_accessor :server_address, :timeout, :cache_directory
   
-  def initialize(server_address = '50.19.106.48:8080', timeout=300)
+  def initialize(server_address = '50.19.106.48:8080', timeout=300, cache_directory='tmp/cache')
     @server_address = server_address
     @timeout = timeout
+    @cache_directory = cache_directory
   end
 
   def data(uuid)
     data = []
-    data = JSON.parse(http_get("GetFeature&typeName=#{uuid}&maxFeatures=10000&outputFormat=json"))
+    data = get_features(uuid)
     
     data
   end
   
   def coordinates(uuid)
-    hash = JSON.parse(http_get("GetFeature&typeName=#{uuid}&maxFeatures=1&outputFormat=json"))
+    hash = get_features(uuid)
     coords = hash['features'][0]['geometry']['coordinates'].join(',').split(',')
     "#{coords[0]},#{coords[1]}"
   end
     
   def feature_fields(uuid)
     fields = []
-    hash = JSON.parse(http_get("GetFeature&typeName=#{uuid}&maxFeatures=1&outputFormat=json"))
+    hash = get_features(uuid)
     hash = hash['features'][0]['properties']
     hash.each do |result|      
       fields.push(result[0])
     end
     
     fields
+  end
+  
+  def cache_filename(uuid)
+    "#{cache_directory}/geoserver_#{uuid.gsub('-','_')}.json"
+  end
+  
+  def get_features(uuid)
+    data = []
+    if !(File.exists? cache_filename(uuid))
+      data = JSON.parse(http_get("GetFeature&typeName=#{uuid}&maxFeatures=10000&outputFormat=json"))
+      begin
+        File.open(cache_filename(uuid), 'w') { |f| f.write(data) }
+      rescue
+      end
+    else
+      File.open(cache_filename(uuid), 'r') { |f| data = eval(f.read) }
+    end
+    data
   end
       
   def http_get(uri)
