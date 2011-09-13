870795b7693863683a4cd940cf1723c357674d97
AdamDotCom <adam@kahtava.com>
2011-09-13 14:31:20 -0600

    Modified recycle script

--- consoles/geonetwork/tomcat_recycle.sh
+++ consoles/geonetwork/tomcat_recycle.sh
@@ -5,6 +5,7 @@
 DIRECTORY=$(cd `dirname $0` && pwd)
 
 ps aux | grep tomcat | grep -v grep | awk '{ print $2 }' | xargs kill -9 2> recycle.log
-/usr/local/tomcat/bin/startup.sh > recycle.log
+cd /usr/local/tomcat/bin/
+./startup.sh > $DIRECTORY/recycle.log
 
 mail -s "Geonetwork's tomcat recycled `date`" adam.kahtava@tesera.com < $DIRECTORY/recycle.log
