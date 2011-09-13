5ed4832daaa3eb290dc466e916b88b7f3198bbd2
AdamDotCom <adam@kahtava.com>
2011-09-13 14:17:59 -0600

    Cron job to recycle GeoNetwork\'s tomcat server

---
+++ consoles/geonetwork/tomcat_recycle.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+[[ "`/usr/bin/tty`" == "not a tty" ]] && . ~/.bash_profile
+
+DIRECTORY=$(cd `dirname $0` && pwd)
+
+ps aux | grep tomcat | grep -v grep | awk '{ print $2 }' | xargs kill -9 2> recycle.log
+/usr/local/tomcat/bin/startup.sh > recycle.log
+
+mail -s "Geonetwork's tomcat recycled `date`" adam.kahtava@tesera.com < $DIRECTORY/recycle.log
