af770264912ef43dcedd798a74626410e6ee02b0
AdamDotCom <adam@kahtava.com>
2011-09-20 07:31:19 -0600

    Added comments to the script that fixes the feature data hyphens to underscores

--- consoles/catalogue/fix_feature_data.rb
+++ consoles/catalogue/fix_feature_data.rb
@@ -1,5 +1,8 @@
 require 'csv'
 
+# Generate a CSV file of offending tablenames through psql
+# COPY tablename FROM pg_tables WHERE tablename LIKE '%-%'; TO '/projects/WEHub/consoles/catalogue/feature_data_to_fix.csv' WITH CSV HEADER;
+ 
 CSV.foreach('feature_data_to_fix.csv') do |row|
   tablename = row[0]
   if !tablename.nil? && tablename != 'tablename'
