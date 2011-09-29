c5c6538c4d3eaa4b97ac41f9283121437df928c5
AdamDotCom <adam@kahtava.com>
2011-09-29 16:15:27 -0400

    Updated script to alter incorrectly named feature data tables

--- consoles/catalogue/fix_feature_data.rb
+++ consoles/catalogue/fix_feature_data.rb
@@ -1,7 +1,7 @@
 require 'csv'
 
 # Generate a CSV file of offending tablenames through psql
-# COPY tablename FROM pg_tables WHERE tablename LIKE '%-%'; TO '/projects/WEHub/consoles/catalogue/feature_data_to_fix.csv' WITH CSV HEADER;
+# COPY (SELECT tablename FROM pg_tables WHERE tablename LIKE '%-%') TO '/projects/WEHub/consoles/catalogue/feature_data_to_fix.csv' WITH CSV HEADER;
  
 CSV.foreach('feature_data_to_fix.csv') do |row|
   tablename = row[0]
