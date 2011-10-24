875b5a78e320c7335c15768bc3f172f966ca40ad
AdamDotCom <adam@kahtava.com>
2011-10-24 18:15:19 -0400

    Cleaning up the git, moved search from services to apps, deleted unused files

--- apps/secure/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Secure::Application.config.secret_token = '1ba53fc3530cc091e39ce878083d5335241630b9334a6c8c6814b4ba63cfdfa051100e5fa447984b7f198ea00b69316e6125c12d067170f8db0974aa82adfd85'
