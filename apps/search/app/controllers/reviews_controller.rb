752c6cc633d39a290a624846c531823b668b5b85
AdamDotCom <adam@kahtava.com>
2012-01-03 12:48:42 -0700

    Updates to create new comments

--- apps/search/app/controllers/reviews_controller.rb
+++ apps/search/app/controllers/reviews_controller.rb
@@ -1,8 +1,23 @@
 class ReviewsController < ApplicationController
 
+  before_filter :verify_logged_in, :only => [:create]  
+  
   def index
     @reviews = catalogue_instance.find_reviews(params[:id])
     render :layout => false
   end
   
+  def new
+    render :partial => 'new'
+  end
+  
+  def create  
+    review = params[:review]
+    review.merge!(:user_id => current_user.id)
+    review.merge!(:uuid => params[:id])
+    
+    response = catalogue_instance.create_review(review)
+
+    render :nothing => true, :status => (response.nil? ? 500 : :ok)
+  end
 end
