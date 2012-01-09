1948307a8b16dccef83f2bfcc4ba933805a886be
AdamDotCom <adam@kahtava.com>
2012-01-09 10:42:00 -0700

    Linking user profiles to reviews

--- apps/search/app/controllers/reviews_controller.rb
+++ apps/search/app/controllers/reviews_controller.rb
@@ -4,6 +4,11 @@ class ReviewsController < ApplicationController
   
   def index
     @reviews = catalogue_instance.find_reviews(params[:id])
+    
+    @users = socialnetwork_instance.users
+    
+    @reviews = augment_users(@reviews, @users) unless @users.nil?
+
     render :layout => false
   end
   
@@ -20,4 +25,22 @@ class ReviewsController < ApplicationController
 
     render :nothing => true, :status => (response.nil? ? 500 : :ok)
   end
+
+  def augment_users(reviews, users)
+    reviews.each_with_index do |r, i|
+      user = find_user(users, r.user_id)
+      reviews[i].merge!(:user => user) unless user.nil?
+    end unless users.nil? || users.empty?
+  end
+  
+  def find_user(users, id)
+    users.each do |u|
+      if u.id.to_i == id.to_i
+        return u
+      end
+    end
+    
+    nil
+  end  
+  
 end
