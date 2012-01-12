06f4ced7b800734d229684027c06007c48c57816
AdamDotCom <adam@kahtava.com>
2012-01-12 09:06:42 -0700

    Fixed a bug with star counts on the search page

--- services/catalogue/app/models/user_review.rb
+++ services/catalogue/app/models/user_review.rb
@@ -19,8 +19,10 @@ class UserReview < ActiveRecord::Base
     ratings = find_ratings_by_uuid(self.uuid)
 
     possible = 0
+    sum = 0
     ratings.each do |r|
       possible = possible + r['votes'].to_i * 5
+      sum = sum + r['votes'].to_i * r['index'].to_i
     end unless ratings.nil?
     
     average = find_average_rating_by_uuid(self.uuid)
@@ -29,7 +31,7 @@ class UserReview < ActiveRecord::Base
     total = find_count_by(self.uuid)
     total = total[0]['count'].to_i unless total.nil?
     
-    { :average => average, :possible => possible, :total => total, :ratings => ratings }
+    { :average => average, :possible => possible, :total => total, :sum => sum, :ratings => ratings }
   end
     
 end
