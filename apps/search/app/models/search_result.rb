e8510c2aaa8589fdf0c6a4c470d1edbfe5d04a98
AdamDotCom <adam@kahtava.com>
2012-02-27 12:32:20 -0700

    Fixed an issue with my property to keyword naming switch

--- apps/search/app/models/search_result.rb
+++ apps/search/app/models/search_result.rb
@@ -1,8 +1,12 @@
 class SearchResult
-  attr_accessor :id, :uuid, :date, :description, :period, :name, :owner, :author, :properties, :coordinates, :review_summary
+  attr_accessor :id, :uuid, :date, :description, :period, :name, :owner, :author, :properties, :keywords, :coordinates, :review_summary
 
   def initialize(uuid)
     @id = @uuid = uuid
+  end
+  
+  def keywords
+    @properties
   end
 
 end
