4b340c078885f50f13b37967db269b668f925627
AdamDotCom <adam@kahtava.com>
2012-03-30 13:41:55 -0600

    Resolved a couple more behaviour issues with the new API builder

--- apps/search/app/controllers/api_controller.rb
+++ apps/search/app/controllers/api_controller.rb
@@ -114,18 +114,20 @@ class ApiController < ApplicationController
           feature_request.merge(@bounds)
           @bounds = "&#{@bounds.to_query}"
         end
-        
-        if params[:layer_north] && !params[:layer_north].empty?
-          @bounds = { :north => params[:layer_north], :east => params[:layer_east], :south => params[:layer_south], :west => params[:layer_west] }
-          feature_request.merge(@bounds)
-          @bounds = "&#{@bounds.to_query}"
-        end
-        
-        @layer = ''
-        if params[:layer]
-          @layer = params[:layer]
-          feature_request[:layer] = @layer
-          @layer = "&layer=#{@layer}"
+
+        if params[:is_external]
+          if params[:layer_north] && !params[:layer_north].empty?
+            @bounds = { :north => params[:layer_north], :east => params[:layer_east], :south => params[:layer_south], :west => params[:layer_west] }
+            feature_request.merge(@bounds)
+            @bounds = "&#{@bounds.to_query}"
+          end
+
+          @layer = ''
+          if params[:layer] && !params[:layer].empty?
+            @layer = params[:layer]
+            feature_request[:layer] = @layer
+            @layer = "&layer=#{@layer}"
+          end          
         end
         
         @extent = ''
@@ -135,11 +137,13 @@ class ApiController < ApplicationController
           @extent = "&#{@extent.to_query}"
         end
         
-        if params[:filter_layer_start] && !params[:filter_layer_start].empty?
-          @extent = { :start => params[:filter_layer_start], :end => params[:filter_layer_end] }
-          feature_request.merge(@extent)
-          @extent = "&#{@extent.to_query}"
-        end
+        if params[:is_external]
+          if params[:filter_layer_start] && !params[:filter_layer_start].empty?
+            @extent = { :start => params[:filter_layer_start], :end => params[:filter_layer_end] }
+            feature_request.merge(@extent)
+            @extent = "&#{@extent.to_query}"
+          end
+        end 
         
         @properties = ''
         if params[:feature_fields] && !params[:feature_fields].empty?
