32d4daafb2681b7e554d0e4332f88fa895cd5b71
AdamDotCom <adam@kahtava.com>
2012-09-05 08:18:31 -0600

    Changed ontology selector to order feature properties by importance

--- apps/search/app/controllers/feature_controller.rb
+++ apps/search/app/controllers/feature_controller.rb
@@ -8,23 +8,30 @@ class FeatureController < ApplicationController
     
   def edit  
     dataset = catalogue_instance.dataset(params[:id])
-    @properties = dataset.properties.split(',')
-    @properties.map! { |p| p.strip }
+    properties = dataset.properties.split(',')
+    properties.map! { |p| p.strip }
     
     @feature_fields_vocabulary = catalogue_instance.vocabulator_feature(params[:id])
 
-    @sample_types = Rails.cache.fetch("vocabulary_sample_types") do
+    @properties = []
+    properties.each_with_index do |property,i|
+      number_of_properties = (@feature_fields_vocabulary[property] && @feature_fields_vocabulary[property].keys) ? @feature_fields_vocabulary[property].keys.length : 0
+      @properties.insert(number_of_properties, { :field_position => i, :name => property, :vocabulary_matches => number_of_properties })
+    end
+    @properties.sort! { |x,y| y[:vocabulary_matches] <=> x[:vocabulary_matches] }
+
+    @sample_types = Rails.cache.fetch("vocabulary_sample_types", :expires_in => 15.minutes) do
       catalogue_instance.vocabulator_sample_types.sort! { |x,y| x.name.downcase <=> y.name.downcase }      
     end
 
-    @units = Rails.cache.fetch("vocabulary_units") do 
+    @units = Rails.cache.fetch("vocabulary_units", :expires_in => 15.minutes) do 
       catalogue_instance.vocabulator_units.sort! { |x,y| x.description.downcase <=> y.description.downcase }
     end
 
-    @variable_names = Rails.cache.fetch("vocabulary_variable_names") do
+    @variable_names = Rails.cache.fetch("vocabulary_variable_names", :expires_in => 15.minutes) do
       catalogue_instance.vocabulator_variable_names.sort! { |x,y| x.name.downcase <=> y.name.downcase }
     end
-    
+
   end
   
   def update
