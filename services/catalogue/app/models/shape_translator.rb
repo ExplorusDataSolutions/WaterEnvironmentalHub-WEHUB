042ad2747ba4a57fb41331372d0789b0dfdef7c8
AdamDotCom <adam@kahtava.com>
2012-11-05 15:01:46 -0700

    Cleaned up error messaging and display on the upload page

--- services/catalogue/app/models/shape_translator.rb
+++ services/catalogue/app/models/shape_translator.rb
@@ -12,19 +12,19 @@ class ShapeTranslator
 
     result = %x[ogr2ogr -f "ESRI Shapefile" -t_srs "EPSG:#{srs}" "#{upload_path(ogr_filenames['shp'])}" "#{upload_path(filenames['shp'])}"]
     if $?.exitstatus == 127
-     raise "ShapeTranslator: Executable ogr2ogr could not be found"
+      raise_error("Executable ogr2ogr could not be found")
     end
 
     result = %x[shp2pgsql -s srs "#{upload_path(ogr_filenames['shp'])}" "#{tablename}" > "#{@filename_sql}"]
     if $?.exitstatus == 127
-     raise "ShapeTranslator: Executable shp2pgsql could not be found"
+      raise_error("Executable shp2pgsql could not be found")
     end
   end
   
   def shape_sql
     sql = ''
     if File.zero?(@filename_sql)
-      raise ArgumentError, "#{@filename} failed to load"
+      raise_error("#{@filename} could not be loaded")
     end
     File.open(@filename_sql, "r") do |file|
       while (row = file.gets)
@@ -46,7 +46,6 @@ class ShapeTranslator
   
   def unzip(filename_upload)  
     result = {}
-    has_dbf = false
 
     Zip::ZipFile.open(filename_upload) do |zip_file|
       zip_file.each do |file|
@@ -55,29 +54,33 @@ class ShapeTranslator
           result.store('shp', file.name)
         end
         if !file.name.match(/.prj$/i).nil?
-          result.store('prj', file.name)        
+          result.store('prj', file.name)
         end
         if !file.name.match(/.dbf$/i).nil?
-          has_dbf = true
+          result.store('dbf', file.name)
         end
 
         zip_file.extract(file, upload_path(file.name)) { true }
       end
     end
     
-    if !has_dbf
-      raise "ShapeTranslator: #{@filename} does not contain a .dbf file"
+    if !result.key?('dbf')
+      raise_error("#{@filename} does not contain a .dbf file")
     end
     if !result.key?('prj')
-      raise "ShapeTranslator: #{@filename} does not contain a .prj file"
+      raise_error("#{@filename} does not contain a .prj file")
     end    
     if !result.key?('shp')
-      raise "ShapeTranslator: #{@filename} does not contain a .shp file"
+      raise_error("#{@filename} does not contain a .shp file")
     end
     
     result
   end
-      
+  
+  def raise_error(message)
+    raise ArgumentError, "#{self.class.name}: #{message}"
+  end
+    
   def upload_path(filename)
     "#{@uploads_directory}/#{filename}"
   end
