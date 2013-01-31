9d59e6b7a5b83dd6712aaba3a9e09ea27cedc658
AdamDotCom <adam@kahtava.com>
2013-01-31 17:20:28 -0500

    uncomited from previous editor, comment dalli_store settings

--- apps/search/config/initializers/session_store.rb
+++ apps/search/config/initializers/session_store.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
-require 'action_dispatch/middleware/session/dalli_store'
-Rails.application.config.session_store :dalli_store, :memcache_server => ['we-rails2-cache.gqmntp.0001.use1.cache.amazonaws.com:11211'], :namespace => 'dev_sessions', :key => '_foundation_session', :expire_after => 7.days
+#require 'action_dispatch/middleware/session/dalli_store'
+#Rails.application.config.session_store :dalli_store, :memcache_server => ['we-rails2-cache.gqmntp.0001.use1.cache.amazonaws.com:11211'], :namespace => 'prod_new_sessions', :key => '_foundation_session', :expire_after => 7.days
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
